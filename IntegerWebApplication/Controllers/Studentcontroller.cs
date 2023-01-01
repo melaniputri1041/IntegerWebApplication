4faecb5f8c38ab61dc63e369276d8bbb250907a4
lailaJong <76423084+lailaJong@users.noreply.github.com>
2023-01-01 16:13:14 +0700

    kedua

---
+++ IntegerWebApplication/Controllers/Studentcontroller.cs
@@ -0,0 +1,35 @@
+﻿using IntegerWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegerWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            var student = new List<Student>();
+
+            student.Add(new Student()
+            {
+
+                id = 1,
+                name = "melan",
+                addres = "Banjarsari",
+                phonenumber = "1234567890"
+            });
+            student.Add(new Student()
+            {
+
+                id = 2,
+                name = "Naily",
+                addres = "Banyumas",
+                phonenumber = "1234567890"
+            });
+
+
+            return View(student);
+        }
+    }
+}
---
+++ IntegerWebApplication/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace IntegerWebApplication.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string addres { get; set; }
+
+        public string phonenumber { get; set; }
+
+    }
+}
